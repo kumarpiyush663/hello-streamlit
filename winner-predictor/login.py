79effba0607b81b5f1d8bce379143e7b76b1ed67
piyushkumar <kumarpiyush663@gmail.com>
2024-03-18 21:36:26 +0530

    user info

---
+++ winner-predictor/login.py
@@ -0,0 +1,16 @@
+# import stauth
+import streamlit as st
+import streamlit_authenticator as stauth
+import yaml
+from yaml.loader import SafeLoader
+with open('config.yaml') as file:
+    config = yaml.load(file, Loader=SafeLoader)
+st.write(config)
+
+authenticator = Authenticate(
+    config['credentials'],
+    config['cookie']['name'],
+    config['cookie']['key'],
+    6,
+    config['preauthorized']
+)
