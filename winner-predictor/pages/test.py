d293587f60a3f7bb27f908344f84a8ced3acf04e
piyushkumar <kumarpiyush663@gmail.com>
2024-03-19 01:53:29 +0530

    changes

--- winner-predictor/pages/test.py
+++ winner-predictor/pages/test.py
@@ -1,11 +1,11 @@
 import streamlit as st
 import Main as ma
+import streamlit as st
+import streamlit_authenticator as stauth
+import yaml
+from yaml.loader import SafeLoader
 
 if 'username' not in st.session_state or st.session_state["username"] == "":
-    # st.divider()
-    # st.write(st.session_state)
-    # st.write(st.session_state["name"])
-    # st.divider()
     st.switch_page("login.py")
 else :
     st.set_page_config(
@@ -16,6 +16,19 @@ else :
     # st.write(st.session_state)
     # st.write(st.session_state["name"])
     # st.divider()
+
+
+    with open('config.yaml') as file:
+        config = yaml.load(file, Loader=SafeLoader)
+    # st.write(config)
+
+    authenticator = stauth.Authenticate(
+        config['credentials'],
+        config['cookie']['name'],
+        config['cookie']['key'],
+        config['cookie']['expiry_days'],
+        config['preauthorized']
+    )
     st.write(f'Welcome *{st.session_state["name"]}*')
     st.write("# Welcome to IPL Winner Predictor! 🏏")
 
