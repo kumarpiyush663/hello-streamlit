df665767d180dc75d141a2cf1a3b8449cf47b40e
piyushkumar <kumarpiyush663@gmail.com>
2024-03-18 21:46:09 +0530

    user info

--- winner-predictor/login.py
+++ winner-predictor/login.py
@@ -25,11 +25,11 @@ name, authentication_status, username = authenticator.login('Login', 'main')
 # elif authentication_status == None:
 #     st.warning('Please enter your username and password')
 
-if st.session_state["authentication_status"]:
-    authenticator.logout('Logout', 'main')
-    st.write(f'Welcome *{st.session_state["name"]}*')
-    st.title('Some content')
-elif st.session_state["authentication_status"] == False:
-    st.error('Username/password is incorrect')
-elif st.session_state["authentication_status"] == None:
-    st.warning('Please enter your username and password')
+# if st.session_state["authentication_status"]:
+#     authenticator.logout('Logout', 'main')
+#     st.write(f'Welcome *{st.session_state["name"]}*')
+#     st.title('Some content')
+# elif st.session_state["authentication_status"] == False:
+#     st.error('Username/password is incorrect')
+# elif st.session_state["authentication_status"] == None:
+#     st.warning('Please enter your username and password')
