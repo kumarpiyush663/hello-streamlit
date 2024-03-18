c419c18c9120a59137c2a7e819ca45dccc5495a1
piyushkumar <kumarpiyush663@gmail.com>
2024-03-18 22:45:03 +0530

    Path change

--- winner-predictor/login.py
+++ winner-predictor/login.py
@@ -30,7 +30,7 @@ if st.session_state["authentication_status"]:
     authenticator.logout('Logout', 'main')
     # st.write(f'Welcome *{st.session_state["name"]}*')
     # st.title('Some content')
-    st.sidebar.page_link("winner-predictor/Main.py")
+    st.sidebar.page_link("Main.py")
 elif st.session_state["authentication_status"] == False:
     st.error('Username/password is incorrect')
 elif st.session_state["authentication_status"] == None:
