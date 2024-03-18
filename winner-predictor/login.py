0a81fcc18690bbef3cde8a5432a6efcd0ab95331
piyushkumar <kumarpiyush663@gmail.com>
2024-03-18 21:52:27 +0530

    changed login changes

--- winner-predictor/login.py
+++ winner-predictor/login.py
@@ -15,7 +15,7 @@ authenticator = stauth.Authenticate(
     config['preauthorized']
 )
 
-name, authentication_status, username = authenticator.login('Login', 'main')
+name, authentication_status, username = authenticator.login('main')
 # if authentication_status:
 #     authenticator.logout('Logout', 'main')
 #     st.write(f'Welcome *{name}*')
