d5ed8b29196900e3d4f5666555ca964bd90d017d
piyushkumar <kumarpiyush663@gmail.com>
2024-03-19 01:00:15 +0530

    changes

--- winner-predictor/pages/test.py
+++ winner-predictor/pages/test.py
@@ -2,16 +2,20 @@ import streamlit as st
 import Main as ma
 
 if 'username' not in st.session_state or st.session_state["username"] == "":
+    st.divider()
+    st.write(st.session_state)
+    st.write(st.session_state["name"])
+    st.divider()
     st.switch_page("pages/login.py")
 else :
     st.set_page_config(
         page_title="Hello",
         page_icon="🏏",
     )
-    st.divider()
-    st.write(st.session_state)
-    st.write(st.session_state["name"])
-    st.divider()
+    # st.divider()
+    # st.write(st.session_state)
+    # st.write(st.session_state["name"])
+    # st.divider()
     st.write(f'Welcome *{st.session_state["name"]}*')
     st.write("# Welcome to IPL Winner Predictor! 🏏")
 
