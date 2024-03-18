4d4eb7a956d75d7950f8b67a08334fbfbdbba599
piyushkumar <kumarpiyush663@gmail.com>
2024-03-19 01:45:17 +0530

    changes

--- winner-predictor/login.py
+++ winner-predictor/login.py
@@ -28,8 +28,8 @@ name, authentication_status, username = authenticator.login('main')
 
 if st.session_state["authentication_status"]:
     authenticator.logout('Logout', 'main')
-    # if st.button("Registered User"):
-    st.switch_page('pages/test.py')
+    if st.button("Registered User"):
+        st.switch_page('pages/test.py')
     # authenticator.logout('Logout', 'main')
     # st.switch_page('pages/3_DataFrame_Demo.py')
     # authenticator.logout('Logout', 'main')
