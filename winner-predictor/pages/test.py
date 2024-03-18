155593d0b8b1f056ac36c5511a1bde02d8763c8e
piyushkumar <kumarpiyush663@gmail.com>
2024-03-19 00:18:02 +0530

    changes

--- winner-predictor/pages/test.py
+++ winner-predictor/pages/test.py
@@ -1,13 +1,16 @@
 import streamlit as st
 import Main as ma
 
-st.set_page_config(
-    page_title="Hello",
-    page_icon="🏏",
-)
-st.write(f'Welcome *{st.session_state["name"]}*')
-st.write("# Welcome to IPL Winner Predictor! 🏏")
+if st.session_state["name"] == 'None':
+    st.switch_page("pages/login.py")
+else :
+    st.set_page_config(
+        page_title="Hello",
+        page_icon="🏏",
+    )
+    st.write(f'Welcome *{st.session_state["name"]}*')
+    st.write("# Welcome to IPL Winner Predictor! 🏏")
 
-if st.button("Registered User"):
-    st.dataframe(ma.get_registered_user())
+    if st.button("Registered User"):
+        st.dataframe(ma.get_registered_user())
 # authenticator.logout('Logout', 'main')
