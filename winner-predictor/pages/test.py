0f5725bda2b45195c85424810861f46a1f4180a7
piyushkumar <kumarpiyush663@gmail.com>
2024-03-18 23:37:57 +0530

    changes

--- winner-predictor/pages/test.py
+++ winner-predictor/pages/test.py
@@ -1,3 +1,8 @@
 import streamlit as st
 
-st.header("Next Page")
+st.set_page_config(
+    page_title="Hello",
+    page_icon="🏏",
+)
+st.write(f'Welcome *{st.session_state["name"]}*')
+st.write("# Welcome to IPL Winner Predictor! 🏏")
