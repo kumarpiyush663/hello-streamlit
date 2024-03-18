8cad5d50dd7b0c654726aac869a3c83c76d5483c
piyushkumar <kumarpiyush663@gmail.com>
2024-03-18 22:10:10 +0530

    user info

--- winner-predictor/login.py
+++ winner-predictor/login.py
@@ -5,7 +5,7 @@ import yaml
 from yaml.loader import SafeLoader
 with open('config.yaml') as file:
     config = yaml.load(file, Loader=SafeLoader)
-# st.write(config)
+st.write(config)
 
 authenticator = stauth.Authenticate(
     config['credentials'],
@@ -16,6 +16,7 @@ authenticator = stauth.Authenticate(
 )
 
 name, authentication_status, username = authenticator.login('main')
+
 # if authentication_status:
 #     authenticator.logout('Logout', 'main')
 #     st.write(f'Welcome *{name}*')
