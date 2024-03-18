2511db690d570db772f8a17f67906a4f858dce5a
piyushkumar <kumarpiyush663@gmail.com>
2024-03-19 02:09:35 +0530

    changes

--- winner-predictor/pages/test.py
+++ winner-predictor/pages/test.py
@@ -7,34 +7,35 @@ from yaml.loader import SafeLoader
 
 # if 'username' not in st.session_state or st.session_state["username"] == "":
 if not st.session_state["authentication_status"]:
-    st.divider()
-    st.write("Inside if")
-    st.write(st.session_state)
-    st.write(st.session_state["name"])
-    st.divider()
+    # st.divider()
+    # st.write("Inside if")
+    # st.write(st.session_state)
+    # st.write(st.session_state["name"])
+    # st.divider()
     st.switch_page("login.py")
 else:
     st.set_page_config(
         page_title="Hello",
         page_icon="🏏",
     )
-    st.divider()
-    st.write("Inside else")
-    st.write(st.session_state)
-    st.write(st.session_state["name"])
-    st.divider()
+    # st.divider()
+    # st.write("Inside else")
+    # st.write(st.session_state)
+    # st.write(st.session_state["name"])
+    # st.divider()
 
-    # with open('config.yaml') as file:
-    #     config = yaml.load(file, Loader=SafeLoader)
-    # # st.write(config)
-    #
-    # authenticator = stauth.Authenticate(
-    #     config['credentials'],
-    #     config['cookie']['name'],
-    #     config['cookie']['key'],
-    #     config['cookie']['expiry_days'],
-    #     config['preauthorized']
-    # )
+    with open('config.yaml') as file:
+        config = yaml.load(file, Loader=SafeLoader)
+    # st.write(config)
+
+    authenticator = stauth.Authenticate(
+        config['credentials'],
+        config['cookie']['name'],
+        config['cookie']['key'],
+        config['cookie']['expiry_days'],
+        config['preauthorized']
+    )
+    authenticator.logout('Logout', 'main')
     st.write(f'Welcome *{st.session_state["name"]}*')
     st.write("# Welcome to IPL Winner Predictor! 🏏")
 
